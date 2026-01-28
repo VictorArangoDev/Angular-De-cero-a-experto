7e452b004fecc6c83f63c8be5d11a4ffa36f1097
Victor Arango <victorarangoquiroz@gmail.com>
2026-01-28 10:24:54 -0500

    funciones basicas

---
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -0,0 +1,29 @@
+
+function addNumbers(a:number, b:number): number{
+    return a + b;
+}
+
+
+const addNumersArrow = (a: number, b : number): string =>{
+    return `${a + b}`;
+}
+
+
+function multiply( firsNumber: number, seconNumber?: number, base: number  = 2){
+    return firsNumber * base;
+}
+
+
+const result: number  = addNumbers( 1, 2 );
+const resul2: string  = addNumersArrow( 1, 2 );
+const multiplyResult: number  = multiply(5);
+
+console.log({ result, resul2, multiplyResult });
+
+
+
+
+
+
+
+export {};
