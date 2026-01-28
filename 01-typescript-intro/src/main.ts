ba358234d3189d2d2ed3f370bcc5743690cdf911
Victor Arango <victorarangoquiroz@gmail.com>
2026-01-28 10:48:33 -0500

    tarea sobre objectos e interfaces

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,8 +1,8 @@
 import './style.css'
 // import './topics/01-basic-types'
 // import './topics/02-object-interface'
-import './topics/03-functions'
-
+// import './topics/03-functions'
+import './topics/04-homework'
 
 const app  = document.querySelector<HTMLDivElement>('#app')!;
 
