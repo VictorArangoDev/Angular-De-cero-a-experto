334ef5979fe03f807112b4a67681203a06ba96a7
Victor Arango <victorarangoquiroz@gmail.com>
2026-01-29 07:59:18 -0500

    clases - constructor -extender clase - priorizar composición sobre herencia

---
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -0,0 +1,44 @@
+
+
+
+export class Person {
+    // public name: string;
+    // private address: string;
+
+    constructor(
+        public firstName: string, 
+        public lastName: string, 
+        private address: string = "No Address"
+    ) {}
+}
+
+
+// export class Hero extends Person{
+    
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string
+
+//     ){
+//         super(realName, 'New York')
+//     }
+// }
+
+export class Hero {
+
+    
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        public person:Person,
+
+    ){}
+}
+
+
+
+const tony  =  new Person('Tony','Stark' ,'New York')
+const ironMan = new Hero('Iron Man', 45, 'Tony', tony);
+console.log(ironMan);
