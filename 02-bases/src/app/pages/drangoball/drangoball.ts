5d03002aab1ee360c3d4eb5d335c7a4f5fc6320a
Victor Arango <victorarangoquiroz@gmail.com>
2026-01-29 16:20:26 -0500

    ng class clases de forma condicional - @if mostrar elementos de forma condicional

--- 02-bases/src/app/pages/drangoball/drangoball.ts
+++ 02-bases/src/app/pages/drangoball/drangoball.ts
@@ -19,6 +19,7 @@ export class Drangoball {
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
     { id: 3, name: 'Piccolo', power: 3000 },
+    { id: 4, name: 'Yamcha', power: 500 },
 
   ]);
 
